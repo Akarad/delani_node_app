511622abfde15d025b0786753b70133e6f7391bb
Akarad <dianaakara@gmail.com>
2020-07-22 14:20:53 +0300

    add elements to requestHandlers file and add files to public folder

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,25 @@
 let exec = require("child_process").exec;
 
 // function for handling the business logic for index.html
-function index (){
+function index (response){
    console.log("Request handler for index was called.")
-   // Function that will delay for 10 seconds before returning the text.
-   var content = "empty";
-
-   exec("ls -lah", function(error, stdout, stderr){
-       content = stdout;
-   })
   
-   return content
+   exec("ls -lah", function(error, stdout, stderr){
+      
+       response.writeHead(200, {"Content-type": "text/plain"});
+       response.write(stdout);
+       response.end(); 
+   })
+
 }
 
 // function for handling the business logic for  portfolio.html
-function portfolio(){
+function portfolio(response){
    console.log("Request for handler for portfolio was called.")
-   return "These are some of our portfolio projects"
+   response.writeHead(200, {"Content-type": "text/plain"});
+   response.write("These are some of our portfolio projects");
+   response.end();
+  
 }
 
 
